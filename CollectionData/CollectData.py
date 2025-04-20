239a875d40a080f095a837141315089a65c14fef
slamDun <112410573+slamDun@users.noreply.github.com>
2025-04-20 12:24:50 -0500

    Updated Structure

---
+++ CollectionData/CollectData.py
@@ -0,0 +1,10 @@
+# from googlenews import GoogleNews
+# from newspapert import Article
+# import pandas as pd
+
+
+# def scrape_news(ticker, num_articles=20):
+#     gn = GoogleNews(lang='en', period='30d')
+#     gn.search(f"{ticker} stock")
+#     articles = []
+#     credible_sources = ['Reuters']
